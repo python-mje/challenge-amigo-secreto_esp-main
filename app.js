6bb1c700aeaf0e6ed0688aa93d5af252cb09c6a9
Matías J. Encina <m81j.encina@gmail.com>
2025-03-15 20:17:33 -0300

    Carga de archivo app.js con logica de programacion

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+  const nombreAmigo = document.getElementById("amigo").value.trim();
+
+  if (!nombreAmigo) {
+    alert("Por favor, inserte un nombre.");
+    return;
+  }
+
+  amigos.push(nombreAmigo);
+  actualizarListaAmigos();
+  document.getElementById("amigo").value = "";
+}
+
+function actualizarListaAmigos() {
+  const lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+
+  amigos.forEach((amigo) => {
+    const li = document.createElement("li");
+    li.textContent = amigo;
+    lista.appendChild(li);
+  });
+}
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("No hay amigos en la lista para sortear.");
+    return;
+  }
+
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  const amigoSorteado = amigos[indiceAleatorio];
+
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = `<li>${amigoSorteado}</li>`;
+}
